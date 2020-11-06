3035619b3d2818c45f307f9d2138472e185d4618
Luan Fernandes <hello@luanfernandes.dev>
2020-11-06 00:24:18 -0300

    error 404

---
+++ src/app/error-404/error-404.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './error-404.component.html',
+})
+export class Error404Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
